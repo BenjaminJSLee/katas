92cb32c92453a27f9c74588c9b0f021cbdaa0168
Benjamin Lee <benjaminjslee@gmail.com>
2020-10-30 00:26:13 +0000

    refactored 10-talking-calender.js: changed the switch statement to an accessible array of months.

--- 10-talking-calendar.js
+++ 10-talking-calendar.js
@@ -1,46 +1,6 @@
 const numToMonth = function(num) {
-  let month = undefined;
-  switch (Number(num)) {
-  case 1:
-    month = "January";
-    break;
-  case 2:
-    month = "February";
-    break;
-  case 3:
-    month = "March";
-    break;
-  case 4:
-    month = "April";
-    break;
-  case 5:
-    month = "May";
-    break;
-  case 6:
-    month = "June";
-    break;
-  case 7:
-    month = "July";
-    break;
-  case 8:
-    month = "August";
-    break;
-  case 9:
-    month = "September";
-    break;
-  case 10:
-    month = "October";
-    break;
-  case 11:
-    month = "November";
-    break;
-  case 12:
-    month = "December";
-    break;
-  default:
-    break;
-  }
-  return month;
+  const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  return months[Number(num) - 1];
 };
 
 const spokenDay = function(day) {
